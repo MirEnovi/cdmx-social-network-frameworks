dfd428f29674bb9a63af5900dfa5f67771378dc3
MirEnovi <garciaivonne11@gmail.com>
2018-09-20 20:55:05 -0500

    add file App, and try gh-page on

---
+++ src/components/Login/Login.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from '../../assets/taco.png';
+import './Login.css';
+import {Button} from 'react-materialize';
+
+class Login extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="Login">
+        <header>
+          <img src={logo} className="Login-logo" alt="logo" />
+        </header>
+        <p className="App-title Login-body">
+          {this.props.titulo}
+        </p>
+        <Button waves='light' className='indigo darken-4 '>Facebook</Button>
+        <Button waves='light' className='red'>Google</Button>
+      </div>
+    );                     
+  }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@
 import React from 'react'; // biblioteca en si
 import ReactDOM from 'react-dom'; // herramienta para el navegador
 import './index.css'; // styles de toda la pagina
-import Login from './Login/login'; // componente Login
+import App from './App'; // Importamos App
 import registerServiceWorker from './registerServiceWorker'; // simula servidor
 
-// import App from './App';
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
 
-//renderizamos componente Login
-ReactDOM.render(<Login />, document.getElementById('root'));
+//renderizamos App
+ReactDOM.render(<App titulo= {"Inicia Sesion"} />, document.getElementById('root'));
 registerServiceWorker();
 
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
